14842eb3f68730547f99a243cdf1782c522282c2
Magdalena Chmura <magdalena.chmura24@gmail.com>
2019-01-20 18:47:44 +0100

    add example

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,10 +1,65 @@
 import { Component } from '@angular/core';
 
+interface Todo {
+  id: number;
+  isDone: boolean;
+  text: string;
+}
 @Component({
   selector: 'app-root',
-  template: ``,
-  styles: []
+  template: `
+    <input
+      placeholder="What needs to be done"
+      [(ngModel)]="value"
+      (keydown.enter)="add()"
+    />
+    <button (click)="add()" [disabled]="!value">ADD</button>
+
+    <ul>
+      <li *ngFor="let todo of todos">
+        <span [class.isDone]="todo.isDone">{{ todo.text }}</span>
+        <button (click)="remove(todo.id)">REMOVE</button>
+        <button (click)="toggle(todo)">TOGGLE</button>
+      </li>
+    </ul>
+  `,
+  styles: [
+    `
+      .isDone {
+        text-decoration: line-through;
+      }
+    `
+  ]
 })
 export class AppComponent {
-  title = 'todo';
+  id = 0;
+  value = '';
+  todos: Todo[] = [
+    {
+      id: this.id++,
+      isDone: false,
+      text: 'Learn Angular'
+    }
+  ];
+  add() {
+    if (!this.value) {
+      return;
+    }
+    const nextTodo: Todo = {
+      id: this.id++,
+      isDone: false,
+      text: this.value
+    };
+
+    this.todos.push(nextTodo);
+    this.value = '';
+  }
+
+  remove(id: number) {
+    this.todos = this.todos.filter(todo => todo.id !== id);
+  }
+
+  toggle(todo: Todo) {
+    todo.isDone = !todo.isDone;
+  }
 }
